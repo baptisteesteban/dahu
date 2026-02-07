e847eb7ead6ca1740ffd57531b0b0742fbe835d8
Baptiste Esteban <baptiste.esteban@epita.fr>
2026-02-07 14:27:31 +0100

    Border based lldt

--- dahu/__init__.py
+++ dahu/__init__.py
@@ -1,5 +1,15 @@
 from .immersion import immersion
 from .pqueue import PQueue
 from .border import add_border, add_median_border
+from .utils import C4, C8, in_domain, clamp
 
-__all__ = ["immersion", "PQueue", "add_border", "add_median_border"]
+__all__ = [
+    "immersion",
+    "PQueue",
+    "add_border",
+    "add_median_border",
+    "C4",
+    "C8",
+    "in_domain",
+    "clamp",
+]
---
+++ dahu/utils.py
@@ -0,0 +1,15 @@
+C4 = ((0, -1), (1, 0), (0, 1), (-1, 0))
+C8 = ((0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1))
+
+
+def in_domain(shape: tuple[int, int], l: int, c: int) -> bool:
+    return l >= 0 and c >= 0 and l < shape[0] and c < shape[1]
+
+
+def clamp(v: int, vmin: int, vmax: int) -> int:
+    if v < vmin:
+        return vmin
+    elif v > vmax:
+        return vmax
+    else:
+        return v
